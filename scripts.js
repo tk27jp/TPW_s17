3ad2b71de8b1d3b9d4ebe6db0f4e0c109b568063
tk27jp <daliaph3929q@gmail.com>
2025-11-24 23:31:07 -0500

    cd

--- scripts.js
+++ scripts.js
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
         location: 'Chimbote, Perú',
         links: {
             github: 'https://github.com/settings/profile',
-            linkedin: 'https://linkedin.com/in/usuario',
-            portfolio: 'https://miportafolio.com',
+            linkedin: 'https://www.linkedin.com/in/dalia-pariachi-herrera-10874739a/',
+            portfolio: 'https://github.com/tk27jp/Taller_web.git',
             email: 'mailto:u23225643@utp.edu.pe',
-            cv: 'https://www.facebook.com/dalia.pariachiherrera?locale=es_LA',
+            cv: 'https://www.canva.com/design/DAG5qEHrcZA/ZKGxRvQVlJRYyII0CRc1-g/view?utm_content=DAG5qEHrcZA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0e2d062fa9',
             instagram: 'https://www.instagram.com/tk__2.7/'
         }
     };
